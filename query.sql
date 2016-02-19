436c5d52c26ced440297fc9e028b75e0194f5e29
Federico Thiella <fthiella@gmail.com>
2016-02-19 16:27:28 +0100

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,1 +1,19 @@
-select * from af_connections;
+/*
+  conn="dbi:SQLite:dbname=test.sqlite3"
+  username=""
+  password=""
+*/
+drop table if exists gardens;
+
+create table gardens (
+	id integer primary key,
+	name varchar(100),
+	city varchar(100)
+);
+
+insert into gardens (name, city) values
+('Gardens By The Bay', 'Singapore'), ('Hyde Park', 'London'),
+('Central Park', 'New York'), ('Villa Borghese', 'Rome'),
+('Princes Street Gardens','Edinburgh');
+
+select * from gardens;
